57f3290c6daf5860589ffa2c9612e11f9d0b505e
preff <lbx1057@gmail.com>
2024-10-12 17:42:41 +0300

    Я бы каждого убил, я б остался здесь один

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let timeoutId;
+let timeoutId1;
 const delay = 3000; // время в миллисекундах
+const delay_button = 100;
 
 const musicPanelArea = document.querySelector('.music_panel_area');
-
+const soundtrackButton = document.querySelector('.soundtrack_button')
 const hidePanel = () => {
     musicPanelArea.classList.add('hidden');
 };
@@ -22,3 +24,21 @@ document.body.addEventListener('mousemove', (event) => {
 window.onload = () => {
     timeoutId = setTimeout(hidePanel, delay);
 };
+
+document.querySelectorAll('.svg_text_panel_button').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.svg_text_panel_button').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
+document.querySelectorAll('.svg_icons_panel_button').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.svg_icons_panel_button').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
